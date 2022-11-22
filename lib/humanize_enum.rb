e2b2ab5ce7727a20390b125c5f0a799b505608fd
Max Buslaev <max@buslaev.net>
2022-11-22 16:36:16 +0300

    Initial commit

---
+++ lib/humanize_enum.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "humanize_enum/version"
+
+module HumanizeEnum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/humanize_enum/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HumanizeEnum
+  VERSION = "0.1.0"
+end
