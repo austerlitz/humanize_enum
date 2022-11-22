819de267c0cec815117d2a2cd02c0f304d1062e9
Max Buslaev <max@buslaev.net>
2022-11-22 21:05:43 +0300

    auto-include in ActiveRecord

--- lib/humanize_enum.rb
+++ lib/humanize_enum.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "humanize_enum/version"
+require_relative 'humanize_enum/version'
+require_relative 'humanize_enum/helpers'
+require 'active_support/lazy_load_hooks'
+
 
 module HumanizeEnum
-  class Error < StandardError; end
-  # Your code goes here...
+  if defined?(ActiveSupport.on_load)
+    ActiveSupport.on_load(:active_record) do
+      self.include Helpers
+    end
+  end
 end
