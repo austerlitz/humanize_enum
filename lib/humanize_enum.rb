5097d032f95edaf091c1c27321a35581ad8df711
Max Buslaev <max@buslaev.net>
2022-11-23 12:35:19 +0300

    bump version

--- lib/humanize_enum.rb
+++ lib/humanize_enum.rb
@@ -4,7 +4,6 @@ require_relative 'humanize_enum/version'
 require_relative 'humanize_enum/helpers'
 require 'active_support/lazy_load_hooks'
 
-
 module HumanizeEnum
   if defined?(ActiveSupport.on_load)
     ActiveSupport.on_load(:active_record) do
