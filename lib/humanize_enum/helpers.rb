315516230118b0f61d5ce5f76fc1965bd7bee70e
Max Buslaev <max@buslaev.net>
2022-11-23 11:46:00 +0300

    omg, sorry

--- lib/humanize_enum/helpers.rb
+++ lib/humanize_enum/helpers.rb
@@ -1,46 +1,51 @@
 module HumanizeEnum
   module Helpers
+    def self.included(base)
+      base.extend ClassMethods
+    end
 
     # Struct to be used in html selects and similar places
     SelectOption = Struct.new(:id, :value, :text, :checked)
 
-    # @return [String] translated value of an enum
-    # @example
-    #   Payment.humanize_enum(:status, :pending) # => 'Pending'
-    def self.humanize_enum(enum_name, enum_value)
-      I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}/#{enum_value}")
-    end
-
-    # @return [Hash<String, String>] hash where key is enum name and value is its translation
-    def self.humanize_enums(enum_name)
-      enum_options(enum_name).map do |enum|
-        [enum.value, enum.text]
-      end.to_h
-    end
-
-    # @return [String] translated enum value of an instance
-    # @example
-    #   payment = Payment.new
-    #   payment.status = 'initial'
-    #   payment.humanize_enum(:status) # => 'Initial'
-    def humanize_enum(enum_name)
-      I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}/#{self[enum_name]}")
-    end
-
-    # @return [Array<SelectOption>] array of structs with :id, :key, :text, :checked
-    def self.enum_options(enum_name)
-      enum_i18n_key = enum_name.to_s.pluralize
-      send(enum_i18n_key).map do |key, val|
-        SelectOption.new(val, key, humanize_enum(enum_name, key), nil)
+    module ClassMethods
+      # @return [String] translated value of an enum
+      # @example
+      #   Payment.humanize_enum(:status, :pending) # => 'Pending'
+      def self.humanize_enum(enum_name, enum_value)
+        I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}/#{enum_value}")
       end
-    end
 
-    # @return [Array<SelectOption>]
-    # @see .enum_options
-    def enum_options(enum_name)
-      enum_i18n_key = enum_name.to_s.pluralize
-      self.class.send(enum_i18n_key).map do |key, val|
-        SelectOption.new(val, key, self.class.humanize_enum(enum_name, key), key == self[enum_name])
+      # @return [Hash<String, String>] hash where key is enum name and value is its translation
+      def self.humanize_enums(enum_name)
+        enum_options(enum_name).map do |enum|
+          [enum.value, enum.text]
+        end.to_h
+      end
+
+      # @return [String] translated enum value of an instance
+      # @example
+      #   payment = Payment.new
+      #   payment.status = 'initial'
+      #   payment.humanize_enum(:status) # => 'Initial'
+      def humanize_enum(enum_name)
+        I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name}/#{self[enum_name]}")
+      end
+
+      # @return [Array<SelectOption>] array of structs with :id, :key, :text, :checked
+      def self.enum_options(enum_name)
+        enum_i18n_key = enum_name.to_s.pluralize
+        send(enum_i18n_key).map do |key, val|
+          SelectOption.new(val, key, humanize_enum(enum_name, key), nil)
+        end
+      end
+
+      # @return [Array<SelectOption>]
+      # @see .enum_options
+      def enum_options(enum_name)
+        enum_i18n_key = enum_name.to_s.pluralize
+        self.class.send(enum_i18n_key).map do |key, val|
+          SelectOption.new(val, key, self.class.humanize_enum(enum_name, key), key == self[enum_name])
+        end
       end
     end
   end
